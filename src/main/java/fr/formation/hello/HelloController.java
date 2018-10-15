ad2fd33e471d5a8d5b76ea7bbbface8f0db99e43
Nicolas Hodicq <nicolas.hodicq@gmail.com>
2018-10-15 10:47:11 +0200

    Return Json on HelloController (#6)

--- src/main/java/fr/formation/hello/HelloController.java
+++ src/main/java/fr/formation/hello/HelloController.java
@@ -2,29 +2,31 @@ package fr.formation.hello;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/hello")
+@Secured("ROLE_USER")
 public class HelloController {
+
+	Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	@GetMapping("/admin")
 	@Secured("ROLE_ADMIN")
-	String sayHelloAdmin() {
-		return "Hello Admin!";
-	}
+	Hello sayHelloAdmin() { return new Hello("Hello Admin!");}
 
 	@GetMapping("/user")
 	@Secured("ROLE_USER")
-	String sayHelloUser() {
-		return "Hello User!";
+	Hello sayHelloUser() {
+		return new Hello("Hello User!");
 	}
 
 	@PostMapping("/upload")
@@ -32,6 +34,12 @@ public class HelloController {
 			throws IOException {
 
 		if (!file.isEmpty()) {
+
+			// Vous pouvez trouvez dans ce post un exemple pour sauvegarder un fichier sur le filesystem
+			// https://grokonez.com/frontend/angular/angular-4-uploadget-multipartfile-tofrom-spring-boot-server
+			logger.info("File Name : " + name);
+			logger.info("File Type : " + file.getContentType());
+
 			byte[] bytes = file.getBytes();
 
 			// For the test, we gave txt files that we can display
