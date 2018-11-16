e9f39b915c69c5791ff3e92c43cbd1bad52f1d6e
micou1830 <micou1830@hotmail.fr>
2018-11-16 13:48:25 +0100

    Fix Communes Test URL

--- src/test/java/fr/formation/geo/CommuneControllerTest.java
+++ src/test/java/fr/formation/geo/CommuneControllerTest.java
@@ -31,7 +31,7 @@ public class CommuneControllerTest {
 		String authorizationHeader = mvcResult.getResponse().getHeader("Authorization");
 
 		mvc.perform(
-				get("/communes/nom?value=Amiens")
+				get("/public/communes/nom?value=Amiens")
 				.header("Authorization", authorizationHeader))
 				.andExpect(status().isOk()
 		)
